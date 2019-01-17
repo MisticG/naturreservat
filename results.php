540f8abe18bca4668e32e7fec175a7da415e6471
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-01-17 14:34:29 +0100

    if sats för att skriva ut... måste snygga till dock

--- results.php
+++ results.php
@@ -1,50 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 
     abstract class animal {
         protected $animalType;
         abstract function makeSound();
+        abstract function animalImage();
     }
 
-    class apa extends animal {
+    class animalApa extends animal {
         private $name;
         public $sound;
 
-        function __construct($animalType, $makeSound) {
+        function __construct($animalType, $makeSound, $animalImage) {
             $this->animalType = $animalType;
             $this->sound = $makeSound;
+            $this->animalImg = $animalImage;
         }
 
         public function animalType() {
-            return $this->animalType;
+            return "APA";
         }
 
         function makeSound() {
-            return "OOOoooOOO";
+            return "OOOoooOOO" . "<br>";
+        }
+
+        function animalImage() {
+            echo "<img src='images/apa.jpg' />" . "<br>";
         }
     }
         
-    class giraff extends animal {
+    class animalGiraff extends animal {
         private $name;
         public $sound;
 
-        function __construct($animalType, $makeSound) {
+        function __construct($animalType, $makeSound, $animalImage) {
             $this->animalType = $animalType;
             $this->sound = $makeSound;
+            $this->animalImg = $animalImage;
         }
 
         public function animalType() {
-            return $this->animalType;
+            return "GIRAFF";
         }
 
         function makeSound() {
-            return "Oui oui";
+            return "Oui oui" . "<br>";
+        }
+
+        function animalImage() {
+            echo "<img src='images/giraff.jpg'/>" . "<br>";
         }
     }
 
-$animalOne = new apa ("Apa", "OOooOOOO");
-$animalTwo = new giraff ("Giraff", "Oui oui");
+    
+$animalOne = new animalApa ("apa", "sound", "image");
+$animalTwo = new animalGiraff ("giraff", "sound", "image");
 
-echo $animalOne->makeSound() . "<br>";
-echo $animalTwo->makeSound();
+if($setPost = $_POST['apor']) {
+    for($i = 1; $i<=$setPost; $i++) {
+        echo $animalOne->animalImage()[$i];
+    }
+    echo $animalOne->makeSound();
+};
+
+if($setPost = $_POST['giraffer']) {
+    for($i = 1; $i<=$setPost; $i++) {
+        echo $animalTwo->animalImage()[$i];
+    }
+    echo $animalTwo->makeSound();
+};
  
 ?>
+
+</body>
+</html>
