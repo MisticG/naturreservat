e256d9aa817c5a8ec95e71ac7862df577e2c3fcc
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-01-17 09:53:25 +0100

    created form

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Naturreservat</h1>
+    </header>
+
+    <form>
+        Apor <input type="number" name="apor">
+        <br>
+        <br>
+        Giraffer <input type="number" name="giraffer">
+        <br>
+        <br>
+        Tigrar <input type="number" name="tigrar">
+        <br>
+        <br>
+        Kokosnötter <input type="number" name="kokos">
+        <br>
+        <br>
+        <input type=button value="Hur många vill du ha?">
+    </form>
+    
+
+</body>
+</html>
